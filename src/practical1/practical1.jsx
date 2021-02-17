0ac795adb3f5d8ad44f69cc3ca29b63432afe478
vikas yadav <vikas.yadav_cs18@gla.ac.in>
2021-02-17 18:04:23 +0530

    done

---
+++ src/practical1/practical1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function practical1() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+import React, { Component } from 'react'
+
+export default class practical1 extends Component {
+    render() {
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
