8895bacfe710088a766f092cf5d432333e4bb736
vikas yadav <vikas.yadav_cs18@gla.ac.in>
2021-02-25 01:02:29 +0530

    upload

---
+++ src/practice/Props and States/ClassPropsState.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ClassPropsState extends Component{
+constructor()
+{
+    this.state=
+    {
+    name:"vikas",
+
+    };
+};
+
+this.setState()
+
+    render()
+        {
+            return(
+                <>
+
+                </>
+            );
+        };
+}
+export default ClassPropsState;
